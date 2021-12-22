a8f27d7d14f1089f338a0d01f312a452d85c2d70
Crowdbotics <team@crowdbotics.com>
2021-12-22 21:42:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "paper_snowflakes_32546",
+  url: "https://paper_snowflakes_32546.botics.co",
+  api: "https://paper_snowflakes_32546.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
